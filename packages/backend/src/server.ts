e1c4d11fb22f8cf9fc20017d19923a206d73e140
Juargo <jorgeretamal.jjra@gmail.com>
2025-02-01 21:57:01 -0300

    format

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -6,10 +6,10 @@ console.log(formatTitle('¡Bienvenido!'))
 const fastify = Fastify({ logger: true })
 
 fastify.get('/', async (_, reply) => {
-	reply.send({ message: '¡Bienvenido a WTFAnimeWatch API!' })
+  reply.send({ message: '¡Bienvenido a WTFAnimeWatch API!' })
 })
 
 fastify.listen({ port: 3000 }, (err, address) => {
-	if (err) throw err
-	console.log(`🚀 Servidor corriendo en ${address}`)
+  if (err) throw err
+  console.log(`🚀 Servidor corriendo en ${address}`)
 })
