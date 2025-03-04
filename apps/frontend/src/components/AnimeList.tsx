1af18a2cc428fb7e08b0d34191df3ddba0fd7f65
Juargo <jorgeretamal.jjra@gmail.com>
2025-03-03 23:54:28 -0300

    fix(lint): se corrigen types

--- apps/frontend/src/components/AnimeList.tsx
+++ apps/frontend/src/components/AnimeList.tsx
@@ -10,19 +10,20 @@ interface Anime {
   type: string
   year: number
   status: string
+  mal_id: number
 }
 
 interface AnimeListProps {
   animes: Anime[]
 }
 
-const AnimeList: React.FC<AnimeListProps> = ({ animes }) => {
+const AnimeList: React.FC<AnimeListProps> = ({ animes = [] }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3 p-4">
       {animes?.length > 0 ? (
         animes
-          .filter((anime) => anime.status !== 'Not yet aired')
-          .map((anime) => <AnimeCard key={anime.mal_id} {...anime} />)
+          .filter((anime: Anime) => anime.status !== 'Not yet aired')
+          .map((anime: Anime) => <AnimeCard key={anime.mal_id} {...anime} />)
       ) : (
         <p>No se encontraron animes.</p>
       )}
